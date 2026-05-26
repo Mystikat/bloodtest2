e933b1753cf4466a8a9879be29f94c6838bf8592
Mysti <mysticat.official@gmail.com>
2026-05-26 14:22:35 -0500

    added important commment :)

--- src/main/java/net/mysti/bloodtest/BloodTest.java
+++ src/main/java/net/mysti/bloodtest/BloodTest.java
@@ -5,6 +5,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//very important comment
 public class BloodTest implements ModInitializer {
 	public static final String MOD_ID = "bloodtest";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
